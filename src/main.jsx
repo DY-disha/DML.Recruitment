90bf6c140ec77b99afed025d47dd8597b3fcbdf7
Dilara Yasmin Disha <130140000+DY-disha@users.noreply.github.com>
2024-12-09 16:58:18 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import App1 from './App1.jsx'
-import App2 from './App2.jsx'
-import App3 from './App3.jsx'
-import App4 from './App4.jsx'
+
 import App5 from './App5.jsx'
 
 createRoot(document.getElementById('root')).render(
